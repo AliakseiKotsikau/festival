f5a8a68a21ddc50ae43feb6915bb412ff6f3519e
user <user@PC-06-329-07.iba>
2018-10-04 10:43:06 +0300

    main

--- events/MainClass.java
+++ events/MainClass.java
@@ -3,8 +3,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		EventInfo event = new EventInfo (100, "11.11.2018","Concert" ))
-		Festival fest = new Festival();
-	
+		Festival fest = new Festival(event);
 		
 
 	}
