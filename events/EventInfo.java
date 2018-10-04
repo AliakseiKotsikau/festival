005c952fd75e56a3e077693a815323063ef3c9e7
user <user@PC-06-329-07.iba>
2018-10-04 10:06:47 +0300

    Exception added

--- events/EventInfo.java
+++ events/EventInfo.java
@@ -5,4 +5,6 @@ import java.util.Date;
 public class EventInfo  {
     private int maxPeople;
     private Date date;
+    
+    
 }
--- events/Festival.java
+++ events/Festival.java
@@ -1,4 +1,16 @@
 package events;
 
-public class Festival extends Event {
+public class Festival {
+	private EventInfo info;
+	private ArrayList users;
+	
+	public void addUser(RegistredUser user) {
+		if(users.size() == info.getMaxPeople())
+			throw new WrongNumberOfPeopleException(); 
+	}
+	
+}
+
+class WrongNumberOfPeopleException extends Exception {
+	
 }
