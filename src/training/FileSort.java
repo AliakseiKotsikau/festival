e40589d4bc11eef7e81e634d1b0fe575af8d9bfc
Tibon <mrtibon@gmail.com>
2018-10-04 23:14:23 +0300

    Sorting files in directories

--- src/training/FileSort.java
+++ src/training/FileSort.java
@@ -1,6 +1,46 @@
 package training;
 
+import java.io.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class FileSort {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String path = reader.readLine();
+
+        ArrayList<File> fileCopy = createDirectories(path);
+        moveFiles(fileCopy,path);
+
+    }
+
+    public static void moveFiles(ArrayList<File> list, String path) throws IOException {
+        for (File file : list) {
+            String typePath = path + "\\" + file.getName().replaceAll(".+\\.","") + "\\" + file.getName();
+            System.out.println(typePath);
+            file.renameTo(new File(typePath));
+        }
+    }
+
+    public static ArrayList<File> createDirectories(String path) {
+        File files = new File(path);
+        ArrayList<File> fileCopy = new ArrayList<>(Arrays.asList(files.listFiles()));
+        HashSet<String> ends = new HashSet<>();
+        for (File file : files.listFiles()) {
+            ends.add(file.getName().replaceAll(".+\\.",""));   // добавление в Set типов файлов
+        }
+
+        for (String s : ends) {                          // создание папок, называющихся типами файлов
+            String tempPath = path + "\\" + s;
+            File newFolder = new File(tempPath);
+            if(!newFolder.exists()) newFolder.mkdir();
+        }
+        return fileCopy;
+    }
+
 
 
 }
