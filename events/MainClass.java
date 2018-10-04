49cde61dcc2a8e3c195f36e28f2755bef021f44c
user <user@PC-06-329-07.iba>
2018-10-04 11:44:06 +0300

    exception

--- events/MainClass.java
+++ events/MainClass.java
@@ -4,8 +4,11 @@ public class MainClass {
 	public static void main(String[] args) {
 		Event event = new Event (100, "11.11.2018","Concert" ))
 		Festival fest = new Festival(event);
+		
 		try {
-			fest.addUser(new RegistredUser(), event);
+			for(int i =0; i<110; i++) {
+				fest.addUser(new RegistredUser(), event);
+			}
 		}catch (WrongNumberOfPeopleException e) {
 			e.getMessage();
 		}
