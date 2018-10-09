d7fe8c3f255f9882156cfdc2b30e5bd03b5ba079
user <user@PC-06-329-07.iba>
2018-10-09 11:08:31 +0300

    Email and phone verification

--- users/RegistredUser.java
+++ users/RegistredUser.java
@@ -3,5 +3,34 @@ package users;
 public class RegistredUser extends AbstractUser {
 	private String login;
 	private String password;
+	private String phoneNumber;
+	private String email;
 	private boolean admin;
+	
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String mail) throws IllegalArgumentException{
+		if(!mail.matches("\\w+@\\w+\\.\\w{2,7}")) {
+			throw new IllegalArgumentException();
+		}
+		this.email = mail;
+	}
+	
+	public void setPhoneNumber(String number) throws IllegalArgumentException {
+		if(!number.matches("(80|\\+375)-?\\d{2}[\\s-]?(\\d{1,3}[\\s-]?)+")) {
+			throw new IllegalArgumentException();
+		}
+		this.phoneNumber = number;
+	}
 }
