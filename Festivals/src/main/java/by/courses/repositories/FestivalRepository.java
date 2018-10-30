afaa839aeb4e65d942a4e0370ef178bf05a83df3
Tibon <mrtibon@gmail.com>
2018-10-30 16:31:38 +0300

    spring support

---
+++ Festivals/src/main/java/by/courses/repositories/FestivalRepository.java
@@ -0,0 +1,11 @@
+package by.courses.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import by.courses.model.Festival;
+
+@Repository
+public interface FestivalRepository extends CrudRepository<Festival, Long> {
+
+}
